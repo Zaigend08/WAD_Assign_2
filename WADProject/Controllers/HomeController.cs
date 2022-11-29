035198a13169c89b80afef8f410c94164afdf23a
Zaigham <Zaigham@DESKTOP-FUANBP7>
2022-11-29 21:36:52 +0500

    Add project files.

---
+++ WADProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WADProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Donor()
+        {
+            return View();
+        }
+        public IActionResult Recipient()
+        {
+            return View();
+        }
+        public IActionResult Blood_Collection()
+        {
+            return View();
+        }
+        public IActionResult Blood_Request()
+        {
+            return View();
+        }
+        public IActionResult Blood_Issued()
+        {
+            return View();
+        }
+        public IActionResult User_Accounts()
+        {
+            return View();
+        }
+        public IActionResult User_Logs()
+        {
+            return View();
+        }
+    }
+}
---
+++ WADProject/wwwroot/js/scripts.js
@@ -0,0 +1,17 @@
+//const items = document.queryselectorall('.navitem')
+//function activelink() {
+//    items.forEach((e) =>
+ //       e.classList.remove('bb'));
+ //   this.classList.add('bb');
+//};
+//}
+//items.forEachEach((e) =>
+//   e.addEventListener('click', activelink));
+
+function openForm(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+
+    var popup = document.getElementById('popupScreen')
+    popup.classList.toggle('active')
+}
